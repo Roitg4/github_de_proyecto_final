c6bdb67b90bc923e931eb482fbecf6170d895977
Roitg <roitgtomas@hotmail.com>
2021-09-28 19:00:10 -0300

    Utilizacion del Auth

--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const ClienteRoutes = express.Router();
-
+const auth = require('../middleware/auth');
 const ClienteController = require('../controllers/cliente.controller');
 
 // de acceso privado
-ClienteRoutes.get('/', ClienteController.principal );//Busca a todos
-ClienteRoutes.get('/buscar/:key/:value', ClienteController.buscar );//Busca uno
-ClienteRoutes.get('/buscar/:id', ClienteController.buscarId );
-ClienteRoutes.put('/editar', ClienteController.editar );//Editar info de cliente
-ClienteRoutes.delete('/eliminar', ClienteController.eliminar );//Eliminar al cliente
+ClienteRoutes.get('/', auth, ClienteController.principal );//Busca a todos
+ClienteRoutes.get('/buscar/:key/:value', auth, ClienteController.buscar );//Busca uno
+ClienteRoutes.get('/buscar/:id', auth, ClienteController.buscarId );
+ClienteRoutes.put('/editar', auth, ClienteController.editar );//Editar info de cliente
+ClienteRoutes.delete('/eliminar', auth, ClienteController.eliminar );//Eliminar al cliente
 
 
 //----------------------------------------------------------------
--- routes/usuario_admin.routes.js
+++ routes/usuario_admin.routes.js
@@ -1,23 +1,21 @@
 const express = require('express');
-/* const auth = require('../middleware/auth'); */
+const auth = require('../middleware/auth');
 const UsuarioRoutes = express.Router();
 
 const UsuarioAdmController = require('../controllers/usuario_admin.controller');
 
 // de acceso privado
-UsuarioRoutes.get('/', UsuarioAdmController.principal );//Busca a todos
-UsuarioRoutes.get('/buscar/:key/:value', UsuarioAdmController.buscar );//Busca uno
-UsuarioRoutes.get('/buscar/:id', UsuarioAdmController.buscarId );
-UsuarioRoutes.put('/editar', UsuarioAdmController.editar );//Editar info de usuario
-UsuarioRoutes.delete('/eliminar', UsuarioAdmController.eliminar );//Eliminar al usuario
-
+UsuarioRoutes.get('/', auth, UsuarioAdmController.principal );//Busca a todos
+UsuarioRoutes.get('/buscar/:key/:value', auth, UsuarioAdmController.buscar );//Busca uno
+UsuarioRoutes.get('/buscar/:id', auth, UsuarioAdmController.buscarId );
+UsuarioRoutes.put('/editar', auth, UsuarioAdmController.editar );//Editar info de usuario
+UsuarioRoutes.delete('/eliminar', auth, UsuarioAdmController.eliminar );//Eliminar al usuario
 
 //----------------------------------------------------------------
 // de acceso publico
 UsuarioRoutes.post('/registro',  UsuarioAdmController.registro );//Crear un usuario
 UsuarioRoutes.post('/login',  UsuarioAdmController.login );//Logearse con un usuario
 UsuarioRoutes.post('/verifyToken',  UsuarioAdmController.verifyToken );
-
 
 
 module.exports = UsuarioRoutes;
